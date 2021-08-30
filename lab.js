417c75b5a654638423b68043cc7ddefb129d3d7c
Goqor1999 <goqor.hunanyan99@gmail.com>
2021-08-31 02:00:03 +0400

    v-card

---
+++ lab.js
@@ -0,0 +1,12 @@
+function cv(){
+ document.getElementById("div1").style.display="none";
+ document.getElementById("div6").style.display="block";
+ document.getElementById("i1").style.display="none";
+ document.getElementById("i2").style.display="block";
+}
+function glxavor(){
+ document.getElementById("div1").style.display="block";
+ document.getElementById("div6").style.display="none";
+ document.getElementById("i1").style.display="block";
+ document.getElementById("i2").style.display="none";
+}
